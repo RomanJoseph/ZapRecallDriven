6b030735e440e59a3e71e0370841b95c00006e4b
Shizuke136 <romanjose3008@gmail.com>
2022-07-20 11:16:07 -0300

    Initial HTML and CSS done

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import InitialPage from "./initialPage/initialPage";
+
+function App(){
+    return <InitialPage/>
+}
+
+ReactDOM.render(App(),document.querySelector(".root"))
---
+++ src/initialPage/initialPage.js
@@ -0,0 +1,11 @@
+export default function InitialPage(){
+    return(
+        <div className="initialPage">
+          <img className="logo" src="assets/logo.png"></img>
+          <h1>ZapRecall</h1>
+          <div className="initGame">
+            <p>Iniciar Recall!</p>
+          </div>
+        </div>
+    )
+}
